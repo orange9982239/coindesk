88b317682236e19217c02019ee32dbb40da9bb2e
orangeUnitlg <orange9982239@outlook.com>
2025-01-15 00:48:14 +0800

    * fix ExchangeRate

--- db/2.createtable.sql
+++ db/2.createtable.sql
@@ -11,10 +11,10 @@ CREATE TABLE [currencies] (
 -- 插入初始可兌換貨幣（以 BTC 為基準）
 INSERT INTO currencies (CurrencyCode, CurrencyName, ExchangeRate, UpdatedAt)
 VALUES
-    ('BTC', N'比特幣', 1.000000,GETDATE()),            -- 本國貨幣，匯率為 1
-    ('USD', N'美元', 0.00001056,GETDATE()),           -- 假設 1 USD = 94,734.4777 BTC，匯率為 1/94,734.4777
-    ('GBP', N'英鎊', 0.00001323,GETDATE()),           -- 假設 1 GBP = 75,557.8511 BTC，匯率為 1/75,557.8511
-    ('EUR', N'歐元', 0.00001097,GETDATE());           -- 假設 1 EUR = 91,109.3682 BTC，匯率為 1/91,109.3682
+    ('BTC', N'比特幣', 1.000000, GETDATE()),
+    ('USD', N'美元', 96167.7374, GETDATE()),
+    ('GBP', N'英鎊', 76700.9831, GETDATE()),
+    ('EUR', N'歐元', 92487.7827, GETDATE());
 
 -- 查詢 currencies 表以確認數據
 SELECT * FROM currencies;
